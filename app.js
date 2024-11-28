f06070210387f2f9d3ddf94a409775e68a087898
Anairda2023 <139265896+Anairda2023@users.noreply.github.com>
2024-11-28 22:14:19 +0000

    docker

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const users = [
+    {nome: "Seu José", idade=94, naturalidade: "Mossoró"},
+    {nome: "Joel", idade=40, naturalidade: "São Pedro"},
+    {nome: "Maria", idade=35, naturalidade: "Macaíba"},
+    {nome: "Alê", idade=150, naturalidade: "Roma"}
+]
+
+app.get('/', (req, res)=>{
+    res.send('Hello World Docker');
+})
+
+app.get('/users', (req, res)=>{
+    res.json(users);
+})
+
+app.listen(port, ()=> {
+    console.log(`Aplicação rodando em appt://localhost:${port}`);
+})
